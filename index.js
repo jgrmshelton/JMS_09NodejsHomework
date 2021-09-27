e5e79539bd60aec96ea6fa6425e24247ad4ead6d
Julia Shelton <87336948+jgrmshelton@users.noreply.github.com>
2021-09-26 21:40:14 -0500

    commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// create HTML with user response to promps
+// write/output HTML file to the app root director (same folder)
+// need Success message after HTML is succesfully created
+// callback to verify there was no error
+// maybe writeFileSync?
+// inquirers .promp() method
+
+const inquirer = require('inquirer');
+const fs = require ('fs');
+
