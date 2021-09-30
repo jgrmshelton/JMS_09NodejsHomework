1e6f69c08bfa8e1ece3968a7ad6433aa6dc0b264
Julia Shelton <87336948+jgrmshelton@users.noreply.github.com>
2021-09-29 22:03:39 -0500

    commit

--- index.js
+++ index.js
@@ -7,69 +7,98 @@
 
 const inquirer = require('inquirer');
 const fs = require ('fs');
+const util = require("util");
+
+// writeFileAsync
+const writeFileAsync = util.promisify(fs.writeFile);
 
 
-const promptQuestions = [
-    {
-        type: 'input',
-        name: 'projectTitle',
-        message: 'Project Title: ',  
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Describe your project: ',  
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Did you make any installations while creating this project?  ',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Application Usage: ', 
-    },
-    {
-        type: 'checkbox',
-        name: 'license',
-        message: 'What license did you use for this project?', 
-        choices: ["Attribution-NonCommercial 4.0 International", "Apache 2.0 license", "Boost Software License 1.0", "BSD 3-Clause License", "GNU GPL v3", "ISC License"],
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'Are there rules for other contributors?', 
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Testing Instructions: ',
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Github Username: ',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Email: ',
-    },
-]
+inquirer
+    .prompt ([
+        {
+            type: 'input',
+            name: 'projectTitle',
+            message: 'Project Title: ',  
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Describe your project: ',  
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Did you make any installations while creating this project?  ',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Application Usage: ', 
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'What license did you use for this project?', 
+            choices: ["Attribution-NonCommercial 4.0 International", "Apache 2.0 license", "Boost Software License 1.0", "BSD 3-Clause License", "GNU GPL v3", "ISC License"],
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Are there rules for other contributors?', 
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Test Instructions: ',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Github Username: ',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Email: ',
+        },
+    ])
 
-function init() {
-    inquirer.prompt(promptQuestions).then(function (data) {
-        const readme = "README.md";
+// .then((response))
+    .then((response) => writeFileAsync("README.md", content (response)))
+    .then(() => console.log("README created"));
 
-        fs.writeFile("README.md", (data), (err) => {
-            if (err) {
-                return console.log(err);
-            }
-
-            console.log('YAY! Your README.md was successfully created.');
-        });
-    });
-}
-
-init();
+const content = function (response) {
+    const readMe = `
+    #Title: ${response.projectTitle}
+    ## Table of Contents
+    1. [Description](#description)
+    </br>
+    2. [Installation](#installation)
+    </br>
+    3. [Application Usage](#usage)
+    </br>
+    4. [License](#license)
+    </br>
+    5. [Contributions] (#contribute)
+    </br>
+    6. [Test Instructions](#test)
+    </br>
+    # <span id="description"></span>
+    # Description
+    ### ${response.description}
+    # <span id="installation"></span>
+    # Installation
+    ### ${response.installation} 
+    # <span id="usage"></span>
+    # Application Usage
+    ### ${response.usage} 
+    # <span id="license"></span>
+    # License
+    ### ${response.license} 
+    # <span id="contribute"></span>
+    # Contributions
+    ### ${response.contribute} 
+    # <span id="test"></span>
+    # Test Instructions
+    ### ${response.test}
+`};
